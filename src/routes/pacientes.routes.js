d3eb9438175e747ef9447cdf49424164029658d1
TheFroy <jarodf_11@hotmail.com>
2021-04-22 21:17:50 -0500

    Nueva ruta de prueba

--- src/routes/pacientes.routes.js
+++ src/routes/pacientes.routes.js
@@ -4,5 +4,8 @@ const pacientesController = require('../controllers/pacientes.controllers')
 
 router.get('/getall',pacientesController.getAll)
 router.get('/getone/:id',pacientesController.getOne)
+router.get('/prueba', (req, res) =>{
+    res.json('Esto es una prueba')
+})
 
 module.exports = router
